b42e8d2c4653d087b190edf04652fbe23cd2eafa
philodindron <pajoucla@gmail.com>
2016-12-10 13:35:50 +0100

    correct poule dindon canard
    voila

--- js/EnemyManager/Poule.js
+++ js/EnemyManager/Poule.js
@@ -10,10 +10,21 @@ function Poule(game, velocity, posX, posY) {
 	this.isSpriteDestroy = false;
 };
 
-Ennemy.prototype.create = function create() {
+Poule.prototype.create = function create() {
+	this.pouleSprite = this.game.add.sprite(this.posX, this.posY, "Poule");
+	this.game.physics.arcade.enable(this.pouleSprite);
+
+	this.pouleSprite.animations.add('walk');
+	this.pouleSprite.enableBody = true;
+	this.pouleSprite.body.velocity.x = this.velocity;
+	this.pouleSprite.animations.play('walk', 10, true);
 };
 
 
-Ennemy.prototype.update = function update() {
+Poule.prototype.update = function update() {
 
+};
+
+Poule.prototype.destroy = function destroy() {
+	this.pouleSprite.destroy();
 };
