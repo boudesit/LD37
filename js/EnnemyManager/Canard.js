ff53bb43422d23dd587107bee13bcc252d7a6d34
philodindron <pajoucla@gmail.com>
2016-12-10 12:18:12 +0100

    Proto ennemy

---
+++ js/EnnemyManager/Canard.js
@@ -0,0 +1,21 @@
+function Canard(game, velocity, posX, posY) {
+	this.game = game;
+	this.canardSprite = null;
+	this.isDead = null;
+	this.velocity = velocity;
+	this.posX = posX;
+	this.posY = posY;
+	this.isDead = false;
+	this.isDraw = false;
+	this.isSpriteDestroy = false;
+};
+
+Ennemy.prototype.create = function create() {
+};
+
+
+Ennemy.prototype.update = function update() {
+	if(!this.isDraw && !this.isDead){
+		this.ennemySprite.body.velocity.x = this.velocity;
+	}
+};
