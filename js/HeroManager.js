3eac9edc503f09b935a8fe9523b198a405f915d9
Quentin <quentindegraeve59@gmail.com>
2016-12-10 15:07:55 +0100

    hero

--- js/HeroManager.js
+++ js/HeroManager.js
@@ -6,6 +6,7 @@ var HeroManager = function(game) {
 	this.posY = 450;
 
 	this.isDead = false;
+	this.life = 3;
 
   this.fireButton = null;
   this.weapon = null;
@@ -16,11 +17,12 @@ HeroManager.prototype = {
 
 		//Sprite
 		this.sprite = this.game.add.sprite(this.posX,this.posY, 'hero_idle');
-		this.sprite.animations.add('idle', [0,1,2,3]);
+		//this.sprite.animations.add('idle', [0,1]);
+		//this.sprite.animations.play('idle', 5, true);
+
 		this.game.physics.arcade.enable(this.sprite);
 		this.sprite.physicsBodyType = Phaser.Physics.ARCADE;
 		this.sprite.enableBody = true;
-		this.sprite.animations.play('idle', 5, true);
 	  this.sprite.body.collideWorldBounds=true;
 
 
@@ -29,38 +31,40 @@ HeroManager.prototype = {
     this.weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
     this.weapon.bulletSpeed = 400;
     this.weapon.trackSprite(this.sprite, 1, 0);
-    this.weapon.rotation =  game.physics.arcade.moveToPointer(weapon, 1000, game.input.activePointer, 500);
+    //this.weapon.rotation =  game.physics.arcade.moveToPointer(this.weapon, 1000, game.input.activePointer, 500);
 
     },
 
     update: function() {
 
 			//Deplacement
-				var force = 10;
+				var force = 4;
 
 				if(game.input.keyboard.isDown(Phaser.Keyboard.A)) {
-				    this.sprite.body.velocity.x -= force;
+				    this.sprite.x -= force;
 				}
 
 				if(game.input.keyboard.isDown(Phaser.Keyboard.D)) {
-				    this.sprite.body.velocity.x += force;
-				}
-
-				if(game.input.keyboard.isDown(Phaser.Keyboard.S)) {
-				    this.sprite.body.velocity.y -= force;
+				    this.sprite.x += force;
 				}
 
 				if(game.input.keyboard.isDown(Phaser.Keyboard.W)) {
-				    this.sprite.body.velocity.y += force;
+				    this.sprite.y -= force;
+				}
+
+				if(game.input.keyboard.isDown(Phaser.Keyboard.S)) {
+				    this.sprite.y += force;
 				}
 
         if ( game.input.activePointer.leftButton.isDown )
         {
+					  console.log('Click Souri');
             this.weapon.fire();
         }
 
 				if ( game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
 				{
+				    console.log('Space');
 						//change weapon
 						this.weapon.fire();
 				}
